2877a8028fd4f40b9001cf0130b6ee0ab2a8d37f
Richard Astbury <Richard.Astbury@gmail.com>
2011-10-28 12:00:53 +0100

    added 'exists' to the blob extensions

--- AzureSugar/AzureSugarBlobExtensions.cs
+++ AzureSugar/AzureSugarBlobExtensions.cs
@@ -62,5 +62,25 @@ namespace Two10.AzureSugar
             }
         }
 
+
+        public static bool Exists(this CloudBlob blob)
+        {
+            try
+            {
+                blob.FetchAttributes();
+                return true;
+            }
+            catch (StorageClientException e)
+            {
+                if (e.ErrorCode == StorageErrorCode.ResourceNotFound)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
     }
 }
